189ac2f9f43e52928540dcdd82a1b7dc2ac07c07
Maximilian <scientificresearch309@gmail.com>
2024-04-22 23:44:20 +0200

    moving the watch list to a new function as a new component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -85,29 +85,7 @@ export const WatchedBox = () => {
       {isOpen2 && (
         <>
           <WatchedSummary watched={watched} />
-
-          <ul className="list">
-            {watched.map((movie) => (
-              <li key={movie.imdbID}>
-                <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                <h3>{movie.Title}</h3>
-                <div>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{movie.imdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{movie.userRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{movie.runtime} min</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <WatchedList watched={watched} />
         </>
       )}
     </div>
@@ -145,5 +123,34 @@ export const WatchedSummary: React.FC<{ watched: ITempWatchedData[] }> = ({
         </p>
       </div>
     </div>
+  );
+};
+
+export const WatchedList: React.FC<{ watched: ITempWatchedData[] }> = ({
+  watched,
+}) => {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <li key={movie.imdbID}>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>⭐️</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+            <p>
+              <span>⏳</span>
+              <span>{movie.runtime} min</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
