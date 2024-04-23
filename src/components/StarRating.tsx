9b968d659a753425f04752d19cea276f23440bba
Maximilian <scientificresearch309@gmail.com>
2024-04-23 16:27:22 +0200

    creating a brand new component => StarRating.tsx and deactivating the current App to continue only this new componet

---
+++ src/components/StarRating.tsx
@@ -0,0 +1,3 @@
+export const StarRating = () => {
+  return <div>Hello</div>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App'
+// import './index.css'
+import { StarRating } from "./components/StarRating";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating></StarRating>
+  </React.StrictMode>
+);
