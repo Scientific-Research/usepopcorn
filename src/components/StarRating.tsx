9ded61956372e9629967c38ce29244fd644fb10c
Maximilian <scientificresearch309@gmail.com>
2024-04-23 16:48:28 +0200

    seciónd style for second container

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -4,10 +4,15 @@ const containerStyle = {
   gap: "16px",
 };
 
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
 export const StarRating = () => {
   return (
     <div style={containerStyle}>
-      <div>
+      <div style={starContainerStyle}>
         {Array.from({ length: 5 }, (_, i) => (
           <span key={i}>S{i + 1}</span>
         ))}
