d25c1d47157ab9c99f588d64529ceb4c5f41baf4
Maximilian <scientificresearch309@gmail.com>
2024-04-24 11:15:14 +0200

    everything is working well with colors!

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { StarRating } from "./components/StarRating";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} color="#fcc419" size={48} />
     {/* <StarRating maxRating={10} /> */}
     {/* we have to define default value in Typescript as undefined, it doesn't accept null as default value! */}
     {/* <StarRating maxRating={undefined} /> */}
