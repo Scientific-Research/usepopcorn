8e7c04918fb3ebfaaeed3dbfcee121903df19b63
Maximilian <scientificresearch309@gmail.com>
2024-05-03 18:52:41 +0200

    creating a new custom hook is called: useLocalStorageState which is reusable, creating a reusable custom hook to set and get the watched movie in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
 } from "./components/Main";
 import { useEffect, useState } from "react";
 import { useMovies } from "./hooks/useMovies";
+import { useLocalStorageState } from "./hooks/useLocalStorageState";
 
 const KEY = "27646d5b";
 
@@ -20,26 +21,29 @@ export default function App() {
   // NOTE: calling the useMovies.tsx as CUSTOM HOOK with two inputs and getting three outputs: in fact, useMovies(query, setSelectedId) send us back an object containing three arguments which we extract them using deconstructuring!
   const { movies, isLoading, error } = useMovies(query, setSelectedId);
 
+  // NOTE: calling this CUSTOM HOOK like a useState() for watched and setWatched:
+  const [watched, setWatched] = useLocalStorageState([], "watched");
+  // const [watched, setWatched] = useState([]);
+
   // const [movies, setMovies] = useState(tempMovieData);
   // const [watched, setWatched] = useState(tempWatchedData);
   // const [watched, setWatched] = useState([]);
-  // const [watched, setWatched] = useState([]);
 
-  // const [watched, setWatched] = useState<IMovieWatchedCombined[]>([]);
-  // NOTE: the initial value here would be getting the item from local storage and display them in browser only for first time when the page reloads. => so, here is the best place to do that!
-  const [watched, setWatched] = useState(() => {
-    const storedValue = localStorage.getItem("watched") as string;
+  // // const [watched, setWatched] = useState<IMovieWatchedCombined[]>([]);
+  // // NOTE: the initial value here would be getting the item from local storage and display them in browser only for first time when the page reloads. => so, here is the best place to do that!
+  // const [watched, setWatched] = useState(() => {
+  //   const storedValue = localStorage.getItem("watched") as string;
 
-    // NOTE: when there is not a watched movie in the arrayat the beginning, it would be null and we get an error, to avoid such error, we use ternary operator to assign an empty bracet[].
-    const parsedValue = storedValue ? JSON.parse(storedValue) : []; // to convert the storedValue as string to an object => we use JSON.parse() function.
-    return parsedValue;
+  //   // NOTE: when there is not a watched movie in the arrayat the beginning, it would be null and we get an error, to avoid such error, we use ternary operator to assign an empty bracet[].
+  //   const parsedValue = storedValue ? JSON.parse(storedValue) : []; // to convert the storedValue as string to an object => we use JSON.parse() function.
+  //   return parsedValue;
 
-    // NOTE: JSON.stringify() => to convert an object to a JSON string
-    // const a = JSON.stringify(storedValue);
+  //   // NOTE: JSON.stringify() => to convert an object to a JSON string
+  //   // const a = JSON.stringify(storedValue);
 
-    // NOTE: JSON.parse() => to convert a JOSN string to an object
-    // const b = JSON.parse(storedValue);
-  });
+  //   // NOTE: JSON.parse() => to convert a JOSN string to an object
+  //   // const b = JSON.parse(storedValue);
+  // });
 
   // NOTE: above, we added the movie to the list of movies and now, we want to store the watched movies to local storage which is setItem(key,value) in Web browsers and only available for current URL => http://localhost:5173/ and not for other one:
   // localStorage.setItem(
@@ -48,14 +52,14 @@ export default function App() {
   // NOTE: this was the first solution to write the local storage code here, the second solution is that to write the code for local storage in a useEffect() below: that's why i comment it out here. Write local storage code in a useEffect is better, because it would be reusable!
   //);
 
-  // NOTE: second solution using useEffect to store the watched movies in local storage in the browser! the first method is above!
-  useEffect(() => {
-    localStorage.setItem(
-      "watched",
-      // NOTE: JSON.stringify([...watched, newWatchedMovie]) => we don't need this here anymore, because we have the updated value for wateched movie as a state variable => watched and we just need to use that here!
-      JSON.stringify(watched)
-    );
-  }, [watched]); // to run this useEffect, each time the watched state variable is updated!
+  // // NOTE: second solution using useEffect to store the watched movies in local storage in the browser! the first method is above!
+  // useEffect(() => {
+  //   localStorage.setItem(
+  //     "watched",
+  //     // NOTE: JSON.stringify([...watched, newWatchedMovie]) => we don't need this here anymore, because we have the updated value for wateched movie as a state variable => watched and we just need to use that here!
+  //     JSON.stringify(watched)
+  //   );
+  // }, [watched]); // to run this useEffect, each time the watched state variable is updated!
 
   // const tempQuery = "interstellar";
   // const query = "interstellar";
---
+++ src/hooks/useLocalStorageState.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+// NOTE: creating a reusable custom hook to set and get the watched movie in local storage
+export const useLocalStorageState = (initialState: unknown[], key: string) => {
+  // const [watched, setWatched] = useState<IMovieWatchedCombined[]>([]);
+  // NOTE: the initial value here would be getting the item from local storage and display them in browser only for first time when the page reloads. => so, here is the best place to do that!
+  // const [watched, setWatched] = useState(() => {
+  const [value, setValue] = useState(() => {
+    // const storedValue = localStorage.getItem("watched") as string;
+    const storedValue = localStorage.getItem(key) as string;
+
+    // NOTE: when there is not a watched movie in the array at the beginning, it would be null and we get an error, to avoid such error, we use ternary operator to assign an empty bracet[].
+    const parsedValue = storedValue ? JSON.parse(storedValue) : []; // to convert the storedValue as string to an object => we use JSON.parse() function.
+    return parsedValue;
+
+    // NOTE: JSON.stringify() => to convert an object to a JSON string
+    // const a = JSON.stringify(storedValue);
+
+    // NOTE: JSON.parse() => to convert a JOSN string to an object
+    // const b = JSON.parse(storedValue);
+  });
+
+  // NOTE: second solution using useEffect to store the watched movies in local storage in the browser! the first method is above!
+  useEffect(() => {
+    localStorage.setItem(
+      // "watched",
+      key,
+      // NOTE: JSON.stringify([...watched, newWatchedMovie]) => we don't need this here anymore, because we have the updated value for wateched movie as a state variable => watched and we just need to use that here!
+      // JSON.stringify(watched)
+      JSON.stringify(value)
+    );
+    // }, [watched]); // to run this useEffect, each time the watched state variable is updated!
+  }, [value, key]); // to run this useEffect, each time the watched state variable is updated!
+  return [value, setValue]; // NOTE: these are the same like watched and setWatched that we send them back to the App.tsx
+};
